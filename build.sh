93110ef549885a56ba14cf139d5ca21ad49ed93b
mthh <matthieu.viry@cnrs.fr>
2018-06-11 16:23:01 +0200

    ...

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 git clone https://github.com/mthh/magrit
 git clone https://github.com/pyinstaller/pyinstaller
 curl -O https://nodejs.org/dist/v8.11.2/node-v8.11.2-win-x64.zip
+curl -O https://dl.nwjs.io/v0.31.1/nwjs-v0.31.1-win-x64.zip
 cp Magrit.spec pyinstaller/Magrit.spec
 cd magrit
 cd magrit_app
@@ -16,3 +17,11 @@ python pyinstaller.py --clean --onefile Magrit.spec
 cp ../dlls/spatialindex_c.dll Magrit/dist/Magrit/
 cp ../dlls/spatialindex-64.dll Magrit/dist/Magrit/
 cd ..
+unzip nwjs-v0.31.1-win-x64.zip
+cp nwclientapp/* nwjs-v0.31.1-win-x64
+mkdir magrit_standalone
+mv nwjs-v0.31.1-win-x64/* magrit_standalone
+rm -rf nwjs-v0.31.1-win-x64/
+cp -r pyinstaller/Magrit magrit_standalone/
+mv magrit_standalone/nw.exe magrit_standalone/launch_magrit.exe
+rm -rf *.zip
